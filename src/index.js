adf8fb7de906bf08563c3b27304c28f2b085ca80
mtayllan <messiastayllan@gmail.com>
2020-01-14 08:22:42 -0300

    display a desktop notification when the price of 1 Bitcoin (BTC) exceeds a user specified amount

--- src/index.js
+++ src/index.js
@@ -7,16 +7,27 @@ const ipc = electron.ipcRenderer
 const notifyBtn = document.getElementById('notifyBtn')
 const price = document.querySelector('h1')
 const targetPrice = document.getElementById('targetPrice')
+var targetPriceVal
+
+const notification = {
+  title: 'BTC Alert',
+  body: 'BTC just beat your target price!',
+  icon: path.join(__dirname, '../assets/images/btc.png')
+}
 
 function getBTC() {
   axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
     .then(res => {
       const cryptos = res.data.BTC.USD
       price.innerHTML = '$' + cryptos.toLocaleString('en')
+
+      if (targetPrice.innerHTML != '' && targetPriceVal < res.data.BTC.USD) {
+        const myNotification = new window.Notification(notification.title, notification)
+      }
     })
 }
 getBTC()
-setInterval(getBTC, 30000)
+setInterval(getBTC, 1000)
 
 notifyBtn.addEventListener('click', (e) => {
   const modalPath = path.join('file://', __dirname, 'add.html')
@@ -36,6 +47,6 @@ notifyBtn.addEventListener('click', (e) => {
 })
 
 ipc.on('targetPriceVal', (e, args) => {
-  const targetPriceVal = Number(args)
+  targetPriceVal = Number(args)
   targetPrice.innerHTML = `$${targetPriceVal.toLocaleString('en')}`
 })
