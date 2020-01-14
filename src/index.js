5dfc45d0edd8f1653b617ac5367d768db4854d9c
mtayllan <messiastayllan@gmail.com>
2020-01-14 08:01:04 -0300

    Get BTC price from cryptocompare min-api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const electron = require('electron')
 const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
+const axios = require('axios')
 
 const notifyBtn = document.getElementById('notifyBtn')
+const price = document.querySelector('h1')
+const targetPrice = document.getElementById('targetPrice')
+
+function getBTC() {
+  axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
+    .then(res => {
+      const cryptos = res.data.BTC.USD
+      price.innerHTML = '$' + cryptos.toLocaleString('en')
+    })
+}
+getBTC()
+setInterval(getBTC, 30000)
 
 notifyBtn.addEventListener('click', (e) => {
   const modalPath = path.join('file://', __dirname, 'add.html')
